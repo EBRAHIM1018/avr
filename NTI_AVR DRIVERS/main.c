4abdef304c330c867b7be07664350da26cb05637
EBRAHIM1018 <127453095+EBRAHIM1018@users.noreply.github.com>
2023-03-12 12:13:22 +0200

    Add files via upload

---
+++ NTI_AVR DRIVERS/main.c
@@ -0,0 +1,18 @@
+/*
+ * NTI_AVR DRIVERS.c
+ *
+ * Created: 02/03/2023 07:46:58 م
+ * Author : Ebrahim Ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
